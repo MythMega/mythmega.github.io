2433a97d65b9fd7d45acac934df2303034320adf
MythMega <mythmegass@gmail.com>
2026-01-24 20:27:43 +0100

    => autoupdate

--- pusher.bat
+++ pusher.bat
@@ -1,4 +1,4 @@
 git add *
-git commit -m "Pokegg 0.6.2 => Fixes"
+git commit -m "=> Autocommit"
 git pull https://github.com/MythMega/mythmega.github.io.git master
 git push https://github.com/MythMega/mythmega.github.io.git master
